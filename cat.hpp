573a54dd573a43dbb6c0d5557f128de41dce73be
labricecat <labricecat@duck.com>
2023-08-19 22:00:20 +0200

    added cat.hpp

---
+++ cat.hpp
@@ -0,0 +1,18 @@
+#ifndef CAT_TUI_HPP
+#define CAT_TUI_HPP
+
+/* the whole TUI framework */ 
+
+#include "buffer.hpp"
+#include "config.hpp"
+#include "drawing.hpp"
+#include "id.hpp"
+#include "input.hpp"
+#include "library.hpp"
+#include "lifespan.hpp"
+#include "signals.hpp"
+#include "terminal.hpp"
+#include "vector.hpp"
+#include "window.hpp"
+
+#endif
